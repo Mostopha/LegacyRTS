b42419c1759958ac70011af7bb992faf5d6cff43
Mostafa Haque <93haque@gmail.com>
2017-06-18 00:09:45 -0400

    Warren fixed exception error

---
+++ Legacy/Assets/Scripts/newEnemyMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class newEnemyMove : MonoBehaviour {
+
+    public Transform target;
+    public Transform self;
+    private NavMeshAgent navigator;
+	// Use this for initialization
+	void Start () {
+        self = this.transform;
+        navigator = this.gameObject.GetComponent<NavMeshAgent>();
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameObject.FindGameObjectWithTag("chosen") != null)
+        {
+            target = GameObject.FindGameObjectWithTag("chosen").transform;
+
+        }
+        else
+        { target = self; }
+
+        navigator.SetDestination(target.position);
+
+
+
+
+    }
+}
