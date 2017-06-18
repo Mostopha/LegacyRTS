9c1de821a9bc211756ba7ef340ef3be7a31a4eb0
Mostafa Haque <93haque@gmail.com>
2017-06-18 01:05:47 -0400

    Selection Indicator

---
+++ Legacy/Assets/Scripts/selectedScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectedScript : MonoBehaviour {
+
+    public GameObject selectiorFeedback;
+
+	// Use this for initialization
+	void Start () {
+        selectiorFeedback = GameObject.Find("selector");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.tag == "chosen")
+        {
+            //selectiorFeedback.SetActive(true);
+
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            //selectiorFeedback.SetActive(false);
+
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+	}
+}
