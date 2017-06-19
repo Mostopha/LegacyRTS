4a703e5f9574b5aa276251ff40988fc4541e18f9
Mostafa Haque <93haque@gmail.com>
2017-06-19 19:28:36 -0400

    Minimap working!

---
+++ Legacy/Assets/Scripts/minimapJumpCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimapJumpCamera : MonoBehaviour {
+
+    Vector3 newCameraPosition;
+    Vector3 viewPortPosiion;
+    float mainCameraHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            viewPortPosiion = GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
+            Ray ray = GetComponent<Camera>().ViewportPointToRay(viewPortPosiion);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                newCameraPosition = hit.point;
+                mainCameraHeight = Camera.main.transform.position.y;
+                Camera.main.transform.position = new Vector3(newCameraPosition.x, mainCameraHeight, newCameraPosition.z);
+            }
+
+
+        }
+	}
+}
